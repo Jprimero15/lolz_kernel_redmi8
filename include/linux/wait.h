23a1e0c833512807e9c444b25d2f6cfd9bb82319
Hugo Lefeuvre <hle@owl.eu.com>
2019-02-07 21:03:52 +0100

    sched/wait: Use freezable_schedule() when possible
    Replace 'schedule(); try_to_freeze();' with a call to freezable_schedule().
    
    Tasks calling freezable_schedule() set the PF_FREEZER_SKIP flag
    before calling schedule(). Unlike tasks calling schedule();
    try_to_freeze() tasks calling freezable_schedule() are not awaken by
    try_to_freeze_tasks(). Instead they call try_to_freeze() when they
    wake up if the freeze is still underway.
    
    It is not a problem since sleeping tasks can't do anything which isn't
    allowed for a frozen task while sleeping.
    
    The result is a potential performance gain during freeze, since less
    tasks have to be awaken.
    
    For instance on a bare Debian vm running a 4.19 stable kernel, the
    number of tasks skipped in freeze_task() went up from 12 without the
    patch to 32 with the patch (out of 448), an increase of > x2.5.
    
    Signed-off-by: Hugo Lefeuvre <hle@owl.eu.com>
    Reviewed-by: Joel Fernandes (Google) <joel@joelfernandes.org>
    Cc: Joel Fernandes <joel@joelfernandes.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Rafael J. Wysocki <rjw@rjwysocki.net>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Link: http://lkml.kernel.org/r/20190207200352.GA27859@behemoth.owl.eu.com.local
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: Jprimero15 <jprimero155@gmail.com>

--- include/linux/wait.h
+++ include/linux/wait.h
@@ -353,7 +353,7 @@ do {									\
 
 #define __wait_event_freezable(wq, condition)				\
 	___wait_event(wq, condition, TASK_INTERRUPTIBLE, 0, 0,		\
-			    schedule(); try_to_freeze())
+			    freezable_schedule())
 
 /**
  * wait_event_freezable - sleep (or freeze) until a condition gets true
@@ -412,7 +412,7 @@ do {									\
 #define __wait_event_freezable_timeout(wq, condition, timeout)		\
 	___wait_event(wq, ___wait_cond_timeout(condition),		\
 		      TASK_INTERRUPTIBLE, 0, timeout,			\
-		      __ret = schedule_timeout(__ret); try_to_freeze())
+		      __ret = freezable_schedule_timeout(__ret))
 
 /*
  * like wait_event_timeout() -- except it uses TASK_INTERRUPTIBLE to avoid
@@ -634,7 +634,7 @@ do {									\
 
 #define __wait_event_freezable_exclusive(wq, condition)			\
 	___wait_event(wq, condition, TASK_INTERRUPTIBLE, 1, 0,		\
-			schedule(); try_to_freeze())
+			freezable_schedule())
 
 #define wait_event_freezable_exclusive(wq, condition)			\
 ({									\
