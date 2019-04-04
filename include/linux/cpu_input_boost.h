eee0a995e4901192f0be42bbb991857f07c175eb
Danny Lin <danny@kdrag0n.dev>
2019-04-03 18:05:21 -0700

    cpu_input_boost: Introduce should_kick_frame_boost function
    Based on kdrag0n/proton_bluecross@1305d13:
    
    This will be expanded upon in subsequent commits and used to determine
    whether or not a frame boost should be invoked based on time elapsed
    since last input event.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    [@0ctobot: Adapted from kdrag0n/proton_bluecross@abe3111]
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    [@jprimero15: fix it a bit]
    Signed-off-by: Jprimero15 <jprimero155@gmail.com>

--- include/linux/cpu_input_boost.h
+++ include/linux/cpu_input_boost.h
@@ -11,12 +11,19 @@
 #ifdef CONFIG_CPU_INPUT_BOOST
 void cpu_input_boost_kick(void);
 void cpu_input_boost_kick_max(unsigned int duration_ms);
+
+bool should_kick_frame_boost(unsigned long timeout_ms);
 #else
 static inline void cpu_input_boost_kick(void)
 {
 }
 static inline void cpu_input_boost_kick_max(unsigned int duration_ms)
 {
+}
+
+static inline bool should_kick_frame_boost(unsigned long timeout_ms)
+{
+	return true;
 }
 #endif
 
