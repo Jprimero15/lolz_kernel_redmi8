936d88f3ec58511377aa17217d0f15dc17f5ab2e
Yuyang Du <duyuyang@gmail.com>
2019-05-06 16:19:37 +0800

    BACKPORT: locking/lockdep: Consolidate lock usage bit initialization
    Lock usage bit initialization is consolidated into one function
    mark_usage(). Trivial readability improvement. No functional change.
    
    Signed-off-by: Yuyang Du <duyuyang@gmail.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: bvanassche@acm.org
    Cc: frederic@kernel.org
    Cc: ming.lei@redhat.com
    Cc: will.deacon@arm.com
    Link: https://lkml.kernel.org/r/20190506081939.74287-22-duyuyang@gmail.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Change-Id: I40e82fcda6c2be0e8859ef068dd796ac91c6f2cd
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: Jprimero15 <jprimero15@aospa.co>

--- kernel/locking/lockdep.c
+++ kernel/locking/lockdep.c
@@ -3490,8 +3490,12 @@ void trace_softirqs_off(unsigned long ip)
 		debug_atomic_inc(redundant_softirqs_off);
 }
 
-static int mark_irqflags(struct task_struct *curr, struct held_lock *hlock)
+static int
+mark_usage(struct task_struct *curr, struct held_lock *hlock, int check)
 {
+	if (!check)
+		goto lock_used;
+
 	/*
 	 * If non-trylock use in a hardirq or softirq context, then
 	 * mark the lock as used in these contexts:
@@ -3535,6 +3539,13 @@ static int mark_irqflags(struct task_struct *curr, struct held_lock *hlock)
 		}
 	}
 
+lock_used:
+	/* mark it as used: */
+	if (!mark_lock(curr, hlock, LOCK_USED))
+		return 0;
+
+	return 1;
+
 out:
 	return 1;
 }
@@ -3577,8 +3588,8 @@ int mark_lock_irq(struct task_struct *curr, struct held_lock *this,
 	return 1;
 }
 
-static inline int mark_irqflags(struct task_struct *curr,
-		struct held_lock *hlock)
+static inline int
+mark_usage(struct task_struct *curr, struct held_lock *hlock, int check)
 {
 	return 1;
 }
@@ -3899,11 +3910,8 @@ static int __lock_acquire(struct lockdep_map *lock, unsigned int subclass,
 	else
 		hlock->acquire_ip_caller = ip_caller;
 
-	if (check && !mark_irqflags(curr, hlock))
-		return 0;
-
-	/* mark it as used: */
-	mark_lock(curr, hlock, LOCK_USED);
+	/* Initialize the lock usage bit */
+	mark_lock(curr, hlock, check);
 
 	/*
 	 * Calculate the chain hash: it's the combined hash of all the
