acb3af4f4acabb3551a439f2ccddf1586681f952
Tanish Manku <tanish2k09.dev@gmail.com>
2019-09-22 10:22:20 -0400

    kcal & fbdev: Add hooks for K-Lapse to work
    These lines of code are enabled only when K-Lapse is enabled in defconfig
    
    Co-authored-by: Tanish Manku <tanish2k09.dev@gmail.com>
    Co-authored-by: kingbri <bdashore3@gmail.com>
    Signed-off-by: Jprimero15 <jprimero155@gmail.com>

--- drivers/video/fbdev/msm/mdss_fb.c
+++ drivers/video/fbdev/msm/mdss_fb.c
@@ -57,6 +57,10 @@
 #include "mdp3_ctrl.h"
 #include "mdss_sync.h"
 
+#ifdef CONFIG_KLAPSE
+#include <linux/klapse.h>
+#endif
+
 #if defined(CONFIG_WPONIT_ADJUST_FUN)
 extern uint32_t white_point_num_x;
 extern uint32_t white_point_num_y;
@@ -314,6 +318,9 @@ static void mdss_fb_set_bl_brightness(struct led_classdev *led_cdev,
 							!mfd->bl_level)) {
 		mutex_lock(&mfd->bl_lock);
 		mdss_fb_set_backlight(mfd, bl_lvl);
+#ifdef CONFIG_KLAPSE
+                set_rgb_slider(bl_lvl);
+#endif
 		mutex_unlock(&mfd->bl_lock);
 	}
 }
