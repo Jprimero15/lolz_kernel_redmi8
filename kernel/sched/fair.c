0c12c7967243964c4358a1c83adb04df08cd257e
Vincent Guittot <vincent.guittot@linaro.org>
2020-05-13 15:55:28 +0200

    sched/fair: Fix unthrottle_cfs_rq() for leaf_cfs_rq list
    Although not exactly identical, unthrottle_cfs_rq() and enqueue_task_fair()
    are quite close and follow the same sequence for enqueuing an entity in the
    cfs hierarchy. Modify unthrottle_cfs_rq() to use the same pattern as
    enqueue_task_fair(). This fixes a problem already faced with the latter and
    add an optimization in the last for_each_sched_entity loop.
    
    Fixes: fe61468b2cb (sched/fair: Fix enqueue_task_fair warning)
    Reported-by Tao Zhou <zohooouoto@zoho.com.cn>
    Signed-off-by: Vincent Guittot <vincent.guittot@linaro.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Phil Auld <pauld@redhat.com>
    Reviewed-by: Ben Segall <bsegall@google.com>
    Link: https://lkml.kernel.org/r/20200513135528.4742-1-vincent.guittot@linaro.org

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5249,7 +5249,8 @@ void unthrottle_cfs_rq(struct cfs_rq *cfs_rq)
 	struct rq *rq = rq_of(cfs_rq);
 	struct cfs_bandwidth *cfs_b = tg_cfs_bandwidth(cfs_rq->tg);
 	struct sched_entity *se;
-        long task_delta, idle_task_delta;
+	long task_delta, idle_task_delta;
+
 	se = cfs_rq->tg->se[cpu_of(rq)];
 
 	cfs_rq->throttled = 0;
@@ -5273,11 +5274,10 @@ void unthrottle_cfs_rq(struct cfs_rq *cfs_rq)
 		if (se->on_rq)
 			break;
 		cfs_rq = cfs_rq_of(se);
-		update_load_avg(cfs_rq, se, 0);
-		se_update_runnable(se);
+		enqueue_entity(cfs_rq, se, ENQUEUE_WAKEUP);
 
 		cfs_rq->h_nr_running += task_delta;
-                cfs_rq->idle_h_nr_running += idle_task_delta;
+		cfs_rq->idle_h_nr_running += idle_task_delta;
 
 		/* end evaluation on encountering a throttled cfs_rq */
 		if (cfs_rq_throttled(cfs_rq))
@@ -5286,6 +5286,9 @@ void unthrottle_cfs_rq(struct cfs_rq *cfs_rq)
 
 	for_each_sched_entity(se) {
 		cfs_rq = cfs_rq_of(se);
+
+		update_load_avg(cfs_rq, se, UPDATE_TG);
+		se_update_runnable(se);
 
 		cfs_rq->h_nr_running += task_delta;
 		cfs_rq->idle_h_nr_running += idle_task_delta;
@@ -5317,20 +5320,6 @@ unthrottle_throttle:
 
 		if (list_add_leaf_cfs_rq(cfs_rq))
 			break;
-	}
-
-	if (!se)
-		add_nr_running(rq, task_delta);
-
-	/*
-	 * The cfs_rq_throttled() breaks in the above iteration can result in
-	 * incomplete leaf list maintenance, resulting in triggering the
-	 * assertion below.
-	 */
-	for_each_sched_entity(se) {
-		cfs_rq = cfs_rq_of(se);
-
-		list_add_leaf_cfs_rq(cfs_rq);
 	}
 
 	assert_list_leaf_cfs_rq(rq);
