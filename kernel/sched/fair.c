e26f112b2d279553a44cc0b296758b68a0809625
Kailash <kailash.sudhakar@gmail.com>
2020-05-27 19:48:27 +0530

    Revert "ANDROID: sched: WALT: Refactor cumulative runnable average fixup"
    This reverts commit 1c847afb1a7c8c1668376eb6a272277501fd3ad5.
    
    Signed-off-by: Jprimero15 <jprimero155@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -12198,7 +12198,6 @@ const struct sched_class fair_sched_class = {
 #endif
 #ifdef CONFIG_SCHED_WALT
 	.fixup_walt_sched_stats	= walt_fixup_sched_stats_fair,
-	.fixup_cumulative_runnable_avg = walt_fixup_cumulative_runnable_avg,
 #endif
 };
 
