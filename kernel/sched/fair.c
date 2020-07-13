884ff16470046b88078f4c49c7dd1cc2d32fb288
tytydraco <tylernij@gmail.com>
2020-07-13 06:35:33 +0530

    sched: Do not check if tasks are cache hot
    With such a small timeslice, we are restricting which tasks can migrate
    CPUs if we use a constant delta here. Since we already use NEXT_BUDDY
    and LAST_BUDDY, we should be covered for recent tasks. Hopefully we see
    energy savings with this change.
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    Signed-off-by: Joshua Primero <jprimero155@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -131,7 +131,7 @@ unsigned int sysctl_sched_child_runs_first __read_mostly = 1;
 unsigned int sysctl_sched_wakeup_granularity = 5000000UL;
 unsigned int normalized_sysctl_sched_wakeup_granularity = 5000000UL;
 
-unsigned int __read_mostly sysctl_sched_migration_cost = 1000000UL;
+unsigned int __read_mostly sysctl_sched_migration_cost;
 
 /*
  * The exponential sliding  window over which load is averaged for shares
