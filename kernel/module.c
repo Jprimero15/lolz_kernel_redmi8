4c0df2d64ec77f1db5e0034bd4a6bd81a5b90e74
Jprimero15 <jprimero155@gmail.com>
2021-04-23 02:57:18 +0000

    kernel: module: Ignore all mismatches
    * lto or the ld.lld is somehow messing up with wlan module so this is the only thing we can do for now (unless we are able to make prima built in on miui or find any proper solution)
    
    Signed-off-by: Jprimero15 <jprimero155@gmail.com>

--- kernel/module.c
+++ kernel/module.c
@@ -3651,6 +3651,10 @@ static int load_module(struct load_info *info, const char __user *uargs,
 	long err;
 	char *after_dashes;
 
+	// TEMP HAX
+	flags |= MODULE_INIT_IGNORE_MODVERSIONS;
+	flags |= MODULE_INIT_IGNORE_VERMAGIC;
+
 	err = module_sig_check(info, flags);
 	if (err)
 		goto free_copy;
