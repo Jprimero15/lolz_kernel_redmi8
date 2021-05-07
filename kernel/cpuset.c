35ffa67a487d448e356a001ca9e8d58d78c0b6fd
Jprimero15 <jprimero155@gmail.com>
2021-05-07 19:32:48 +0000

    [DO NOT MERGE]kernel: cpuset: Set values for sdm439 soc
    * our big.LITTLE is kinda inverted so set the correct values for it
    * let alone top-app since it uses all cores anyways
    
    Signed-off-by: Jprimero15 <jprimero155@gmail.com>

--- kernel/cpuset.c
+++ kernel/cpuset.c
@@ -1788,13 +1788,13 @@ static ssize_t cpuset_write_resmask_wrapper(struct kernfs_open_file *of,
 #ifdef CONFIG_CPUSET_ASSIST
 	static struct cs_target cs_targets[] = {
 		/* Little-only cpusets go first */
-		{ "background",		"0-1"},
-		{ "audio-app",		"0-3"},
-		{ "system-background", 	"0-3"},
-		{ "restricted",		"0-3"},
+		{ "background",		"4-5"},
+		{ "audio-app",		"4-7"},
+		{ "system-background", 	"4-7"},
+		{ "restricted",		"4-7"},
 		{ "top-app",		"0-7"},
-		{ "foreground",		"0-3,6-7"},
-		{ "camera-daemon",	"0-3,6-7"},
+		{ "foreground",		"4-7,2-3"},
+		{ "camera-daemon",	"4-7,2-3"},
 	};
 	struct cpuset *cs = css_cs(of_css(of));
 	int i;
