3b70f25cb1f48bcc3970440f58dfef5bf01f19a5
Jprimero15 <jprimero155@gmail.com>
2021-05-07 11:54:26 +0000

    kernel: fork: Reduce task_is_zygote boosting to 50ms
    * this is fairly enough.. 100ms is too much
    
    Signed-off-by: Jprimero15 <jprimero155@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -2107,8 +2107,8 @@ long _do_fork(unsigned long clone_flags,
 #ifdef CONFIG_CPU_INPUT_BOOST
 	/* Boost CPU to the max for 50 ms when userspace launches an app */
 	if (task_is_zygote(current)) {
-		cpu_input_boost_kick_max(100);
-		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 100);
+		cpu_input_boost_kick_max(50);
+		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 50);
 	}
 #endif
 
