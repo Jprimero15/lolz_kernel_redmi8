753e5440beffd8e08cb90160f3508539ac73846a
Joshua Primero <jprimero155@gmail.com>
2021-06-11 09:33:08 +0000

    Revert "kernel: sched: Mitigate non-boosted tasks preempting boosted tasks"
    This reverts commit 9f89549ea2fc495012ed0cfa7a2b754b899f388b.
    
    Signed-off-by: Jprimero15 <jprimero155@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7266,7 +7266,6 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 	int isolated_candidate = -1;
 	int prev_cpu = task_cpu(p);
 	bool next_group_higher_cap = false;
-	struct task_struct *curr_tsk;
 
 	*backup_cpu = -1;
 
@@ -7642,13 +7641,6 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 	 *   a) ACTIVE CPU: target_cpu
 	 *   b) IDLE CPU: best_idle_cpu
 	 */
-	if (target_cpu != -1 && !idle_cpu(target_cpu) &&
-			best_idle_cpu != -1) {
-		curr_tsk = READ_ONCE(cpu_rq(target_cpu)->curr);
-		if (curr_tsk && schedtune_task_boost_rcu_locked(curr_tsk)) {
-			target_cpu = best_idle_cpu;
-		}
-	}
 
 	if (target_cpu == -1)
 		target_cpu = prefer_idle
