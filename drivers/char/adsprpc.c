dd4900bf924d038f4f14f43566fabf691c949993
Panchajanya1999 <panchajanya@azure-dev.live>
2021-10-10 18:58:28 +0530

    drivers/char: adsprpc: Remove Qcom's PM_QoS implementation
    Qualcomm's QoS implementation wastes a significant power from
    CPU cycles.
    Scrap the QoS bits and save a bit power without hurting any
    functionality.
    
    Change-Id: I1de3563d9c99ba863f10a90a900d290bdd8e6b79
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>
    [Jprimero15: Nuke qos mutex also]
    Signed-off-by: Jprimero15 <jprimero155@gmail.com>

--- drivers/char/adsprpc.c
+++ drivers/char/adsprpc.c
@@ -49,7 +49,6 @@
 #include "adsprpc_shared.h"
 #include <soc/qcom/ramdump.h>
 #include <linux/debugfs.h>
-#include <linux/pm_qos.h>
 #include <linux/stat.h>
 
 #define TZ_PIL_PROTECT_MEM_SUBSYS_ID 0x0C
@@ -415,9 +414,7 @@ struct fastrpc_file {
 	struct hlist_head perf;
 	struct dentry *debugfs_file;
 	struct mutex perf_mutex;
-	struct pm_qos_request pm_qos_req;
-	int qos_request;
-	struct mutex pm_qos_mutex;
+	int  qos_request;
 	struct mutex map_mutex;
 	struct mutex fl_map_mutex;
 	int refcount;
@@ -3258,7 +3255,6 @@ static int fastrpc_file_free(struct fastrpc_file *fl)
 	mutex_destroy(&fl->perf_mutex);
 	mutex_destroy(&fl->fl_map_mutex);
 	mutex_destroy(&fl->map_mutex);
-	mutex_destroy(&fl->pm_qos_mutex);
 	kfree(fl);
 	return 0;
 }
@@ -3268,8 +3264,6 @@ static int fastrpc_device_release(struct inode *inode, struct file *file)
 	struct fastrpc_file *fl = (struct fastrpc_file *)file->private_data;
 
 	if (fl) {
-		if (fl->qos_request && pm_qos_request_active(&fl->pm_qos_req))
-			pm_qos_remove_request(&fl->pm_qos_req);
 		if (fl->debugfs_file != NULL)
 			debugfs_remove(fl->debugfs_file);
 		fastrpc_file_free(fl);
@@ -3760,7 +3754,6 @@ static int fastrpc_device_open(struct inode *inode, struct file *filp)
 	hlist_add_head(&fl->hn, &me->drivers);
 	spin_unlock(&me->hlock);
 	mutex_init(&fl->perf_mutex);
-	mutex_init(&fl->pm_qos_mutex);
 	return 0;
 }
 
@@ -3850,7 +3843,6 @@ static int fastrpc_internal_control(struct fastrpc_file *fl,
 {
 	struct fastrpc_apps *me = &gfa;
 	int err = 0;
-	int latency;
 
 	VERIFY(err, !IS_ERR_OR_NULL(fl) && !IS_ERR_OR_NULL(fl->apps));
 	if (err)
@@ -3860,22 +3852,7 @@ static int fastrpc_internal_control(struct fastrpc_file *fl,
 		goto bail;
 
 	switch (cp->req) {
-	case FASTRPC_CONTROL_LATENCY:
-		latency = cp->lp.enable == FASTRPC_LATENCY_CTRL_ENB ?
-			fl->apps->latency : PM_QOS_DEFAULT_VALUE;
-		VERIFY(err, latency != 0);
-		if (err)
-			goto bail;
-		mutex_lock(&fl->pm_qos_mutex);
-		if (!fl->qos_request) {
-			pm_qos_add_request(&fl->pm_qos_req,
-				PM_QOS_CPU_DMA_LATENCY, latency);
-			fl->qos_request = 1;
-		} else
-			pm_qos_update_request(&fl->pm_qos_req, latency);
-		mutex_unlock(&fl->pm_qos_mutex);
-		break;
-	case FASTRPC_CONTROL_SMMU:
+  	case FASTRPC_CONTROL_SMMU:
 		if (!me->legacy)
 			fl->sharedcb = cp->smmu.sharedcb;
 		break;
