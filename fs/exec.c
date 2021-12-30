fe1428913aa74e68b4b5d6ef5e1c7db6dc821f4f
Jprimero15 <jprimero155@gmail.com>
2021-12-30 07:30:43 +0000

    kernel: Bring Back Zygote Boosting
    Revert "[SQUASH]: treewide: Fully Nuke zygote top app boostings"
    
    This reverts commit 940e3b929d1b8421566d5c96910a0f7543028b57.
    
    Signed-off-by: Jprimero15 <jprimero155@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -72,6 +72,17 @@ int suid_dumpable = 0;
 static LIST_HEAD(formats);
 static DEFINE_RWLOCK(binfmt_lock);
 
+#define HWCOMPOSER_BIN_PREFIX "/vendor/bin/hw/android.hardware.graphics.composer"
+#define ZYGOTE32_BIN "/system/bin/app_process32"
+#define ZYGOTE64_BIN "/system/bin/app_process64"
+static struct task_struct *zygote32_task;
+static struct task_struct *zygote64_task;
+
+bool task_is_zygote(struct task_struct *task)
+{
+	return task == zygote32_task || task == zygote64_task;
+}
+
 void __register_binfmt(struct linux_binfmt * fmt, int insert)
 {
 	BUG_ON(!fmt);
@@ -1793,6 +1804,19 @@ static int do_execveat_common(int fd, struct filename *filename,
 	if (is_su && capable(CAP_SYS_ADMIN)) {
 		current->flags |= PF_SU;
 		su_exec();
+	}
+
+	if (capable(CAP_SYS_ADMIN)) {
+		if (unlikely(!strcmp(filename->name, ZYGOTE32_BIN)))
+			zygote32_task = current;
+		else if (unlikely(!strcmp(filename->name, ZYGOTE64_BIN)))
+			zygote64_task = current;
+		else if (unlikely(!strncmp(filename->name,
+					   HWCOMPOSER_BIN_PREFIX,
+					   strlen(HWCOMPOSER_BIN_PREFIX)))) {
+			current->flags |= PF_PERF_CRITICAL;
+			set_cpus_allowed_ptr(current, cpu_perf_mask);
+		}
 	}
 
 	/* execve succeeded */
