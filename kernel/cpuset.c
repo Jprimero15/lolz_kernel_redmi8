089df91a842e114ec3da1b1b9f8f323007ea6ada
zeta96 <ahmad.zainul857@gmail.com>
2022-01-05 19:00:42 +0000

    sched/uclamp: uclamp_assist: add missing audio, restricted app and tune it for every cgroup
    *  reference and cherry pick from
    https://github.com/dlwlrma123/xiaomi_kernel_surya/commit/c4129f0b62840fe62c04959099f806855186c752#diff-e217778d5ec8af50cc7b56b8f2ee3b103e6da6da0bb7bc9d1d594d711b500fe1,
    https://github.com/dlwlrma123/xiaomi_kernel_surya/commit/aea50152c5cb537c459ed8c211bbef83a6b6858f#diff-e217778d5ec8af50cc7b56b8f2ee3b103e6da6da0bb7bc9d1d594d711b500fe1,
    https://github.com/dlwlrma123/xiaomi_kernel_surya/commit/6ac4b63e33d255ec3c5aa261a01874a5a75a7803#diff-e217778d5ec8af50cc7b56b8f2ee3b103e6da6da0bb7bc9d1d594d711b500fe1,
    https://github.com/dlwlrma123/xiaomi_kernel_surya/commit/0e7d40635729f3c3d2b2d79a02a2d7c80bf85753#diff-e217778d5ec8af50cc7b56b8f2ee3b103e6da6da0bb7bc9d1d594d711b500fe1,
    https://github.com/dlwlrma123/xiaomi_kernel_surya/commit/0e7d40635729f3c3d2b2d79a02a2d7c80bf85753#diff-e217778d5ec8af50cc7b56b8f2ee3b103e6da6da0bb7bc9d1d594d711b500fe1,
    https://github.com/dlwlrma123/xiaomi_kernel_surya/commit/426d6697611b44a48775005c72ca519e12e89a8d#diff-e217778d5ec8af50cc7b56b8f2ee3b103e6da6da0bb7bc9d1d594d711b500fe1,
    https://github.com/dlwlrma123/xiaomi_kernel_surya/commit/66a0eb43ebbdda0b2594d04ffd64f79b539f88b9#diff-e217778d5ec8af50cc7b56b8f2ee3b103e6da6da0bb7bc9d1d594d711b500fe1
    
    [Jprimero15: Align the values properly]
    
    Signed-off-by: Jprimero15 <jprimero155@gmail.com>

--- kernel/cpuset.c
+++ kernel/cpuset.c
@@ -2082,11 +2082,13 @@ static void uclamp_set(struct kernfs_open_file *of,
 	const char *cs_name = cs->css.cgroup->kn->name;
 
 	static struct ucl_param tgts[] = {
-		{"top-app",		"35", "100", 0, 1},
-		{"foreground",		"20",  "60", 0, 0},
-		{"background",		"20", "100", 0, 0},
-		{"system-background",	 "0",  "40", 0, 0},
-		{"camera-daemon",	"40", "100", 0, 0},
+		{"audio-app",           "0",  "50",  0, 0},
+		{"top-app",             "35", "100", 0, 1},
+		{"foreground",          "0",  "100", 0, 1},
+		{"restricted",          "0",  "50",  0, 0},
+		{"background",          "0",  "50",  0, 0},
+		{"system-background",   "0",  "50",  0, 0},
+		{"camera-daemon",       "0",  "100", 0, 1},
 	};
 
 	if (!strcmp(current->comm, "init")) {
