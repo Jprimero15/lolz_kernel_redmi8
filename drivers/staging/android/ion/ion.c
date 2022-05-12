638f5831b91cdcfe954fee11a4002d17de395f3f
Sultan Alsawaf <sultan@kerneltoast.com>
2022-05-11 23:24:51 -0700

    ion: Limit concurrency of workqueues freeing buffers asynchronously
    Unbound workqueues do not participate in workqueue concurrency management,
    and users of unbound workqueues are instead expected to handle concurrency
    management themselves. This is problematic for ION since there's no limit
    on the concurrency of buffer-free workers, meaning that they can starve
    other SCHED_NORMAL processes which have the same nice value of zero or a
    higher nice value.
    
    The default number of workers allowed to run concurrently on *each* CPU in
    an unbound workqueue's allowed CPUs is WQ_DFL_ACTIVE, which is a whopping
    256 workers *per CPU*. This is way too high.
    
    Reduce the number of concurrent buffer free workers to just one per CPU to
    prevent them from starving other processes. Note that this cannot be done
    by simply passing 1 to @max_active in alloc_workqueue() because doing so
    with WQ_UNBOUND results in the workqueue becoming implicitly ordered and
    thus only allowing a single worker to run at a time within the entire
    workqueue. Instead, this is achieved using workqueue_set_max_active(),
    which is the designated override for the implicit ordering quirk.
    
    Also, remove WQ_CPU_INTENSIVE since it's redundant when WQ_UNBOUND is set.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Jprimero15 <jprimero155@gmail.com>

--- drivers/staging/android/ion/ion.c
+++ drivers/staging/android/ion/ion.c
@@ -652,9 +652,10 @@ static const struct file_operations ion_fops = {
 void ion_add_heap(struct ion_device *idev, struct ion_heap *heap)
 {
 	if (heap->flags & ION_HEAP_FLAG_DEFER_FREE) {
-		heap->wq = alloc_workqueue("%s", WQ_UNBOUND | WQ_MEM_RECLAIM |
-					   WQ_CPU_INTENSIVE, 0, heap->name);
+		heap->wq = alloc_workqueue("%s", WQ_UNBOUND | WQ_MEM_RECLAIM, 0,
+					   heap->name);
 		BUG_ON(!heap->wq);
+		workqueue_set_max_active(heap->wq, 1);
 	}
 
 	if (heap->ops->shrink)
