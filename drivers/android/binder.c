885597ef57901b2296acc13a0043f6565d11b703
Carlos Llamas <cmllamas@google.com>
2022-05-17 18:58:17 +0000

    UPSTREAM: binder: fix potential UAF of target_{proc,thread}
    Commit 9474be34a727 ("binder: add failed transaction logging info")
    dereferences target_{proc,thread} after they have been potentially
    freed by binder_proc_dec_tmpref() and binder_thread_dec_tmpref().
    
    This patch delays the release of the two references after their last
    usage. Fixes the following two errors reported by smatch:
    
      drivers/android/binder.c:3562 binder_transaction() error: dereferencing freed memory 'target_proc'
      drivers/android/binder.c:3563 binder_transaction() error: dereferencing freed memory 'target_thread'
    
    Fixes: 9474be34a727 ("binder: add failed transaction logging info")
    Reported-by: Dan Carpenter <dan.carpenter@oracle.com>
    Acked-by: Todd Kjos <tkjos@google.com>
    Signed-off-by: Carlos Llamas <cmllamas@google.com>
    Link: https://lore.kernel.org/r/20220517185817.598872-1-cmllamas@google.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    (cherry picked from commit dafa5e9ab8b5b31d64c0ded188d95f8bb6be9746)
    Change-Id: I1557b4fcc6eece76dc49247c01baa8b089dec54e
    Signed-off-by: xiaomei.li <xiaomei.li@unisoc.com>
    Signed-off-by: Carlos Llamas <cmllamas@google.com>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -3881,10 +3881,6 @@ err_bad_call_stack:
 err_empty_call_stack:
 err_dead_binder:
 err_invalid_target_handle:
-	if (target_thread)
-		binder_thread_dec_tmpref(target_thread);
-	if (target_proc)
-		binder_proc_dec_tmpref(target_proc);
 	if (target_node) {
 		binder_dec_node(target_node, 1, 0);
 		binder_dec_node_tmpref(target_node);
@@ -3895,6 +3891,11 @@ err_invalid_target_handle:
 		     proc->pid, thread->pid, return_error, return_error_param,
 		     (u64)tr->data_size, (u64)tr->offsets_size,
 		     return_error_line);
+
+	if (target_thread)
+		binder_thread_dec_tmpref(target_thread);
+	if (target_proc)
+		binder_proc_dec_tmpref(target_proc);
 
 #ifdef CONFIG_ANDROID_BINDER_LOGS
 	{
