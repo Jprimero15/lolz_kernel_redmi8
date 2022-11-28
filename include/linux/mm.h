21629d6a0be25f47bf65382fa477c03c9867842e
John Galt <johngaltfirstrun@gmail.com>
2022-11-28 13:34:19 -0500

    Revert "mm: process reclaim: vmpressure based process reclaim"
    - This reverts commit 1bddbcf as QCOM vmpressure driven process reclaim is redundant compared to Linux PPR which meets userspace dependencies.
    
    Change-Id: Ib1407525dcce77b16679be32879bb26fe97139e8
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    [Jprimero15: Adapted for k4.9]
    Signed-off-by: Jprimero15 <jprimero15@aospa.co>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -2687,21 +2687,5 @@ static inline void setup_nr_node_ids(void) {}
 
 extern int want_old_faultaround_pte;
 
-#ifdef CONFIG_PROCESS_RECLAIM
-struct reclaim_param {
-	struct vm_area_struct *vma;
-	/* Number of pages scanned */
-	int nr_scanned;
-	/* max pages to reclaim */
-	int nr_to_reclaim;
-	/* pages reclaimed */
-	int nr_reclaimed;
-};
-extern struct reclaim_param reclaim_task_anon(struct task_struct *task,
-		int nr_to_reclaim);
-extern int reclaim_pte_range(pmd_t *pmd, unsigned long addr,
-				unsigned long end, struct mm_walk *walk);
-#endif
-
 #endif /* __KERNEL__ */
 #endif /* _LINUX_MM_H */
