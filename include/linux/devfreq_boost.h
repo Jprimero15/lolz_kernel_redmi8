6f76c03cad27a70315e4187b41b5f83a495a6fdb
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2023-01-15 20:54:53 +0600

    devfreq: Sync and adapt to floral's devfreq boost
    This syncs devfreq boost to Sultan Alsawaf's Floral kernel repository [1]
    and adapts changes to our existing driver.
    
    [1]:
    (a) https://github.com/kerneltoast/android_kernel_google_floral/commit/f3860ae3cfd89af6719d59aa92d9ff73f96d9251
    (b) https://github.com/kerneltoast/android_kernel_google_floral/commit/8db02ebfa4e15e287beb8d11f6b439907ad83819
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    
     Conflicts:
    	drivers/devfreq/devfreq_boost.c
    	include/linux/devfreq_boost.h
    
    [Jprimero15: fast-forward, use normal kthread, remove kprofule things]
    Signed-off-by: Jprimero15 <jprimero15@aospa.co>

--- include/linux/devfreq_boost.h
+++ include/linux/devfreq_boost.h
@@ -1,14 +1,15 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 /*
- * Copyright (C) 2018-2019 Sultan Alsawaf <sultan@kerneltoast.com>.
+ * Copyright (C) 2018-2021 Sultan Alsawaf <sultan@kerneltoast.com>.
  */
 #ifndef _DEVFREQ_BOOST_H_
 #define _DEVFREQ_BOOST_H_
 
 #include <linux/devfreq.h>
+#include <linux/types.h>
 
 enum df_device {
-	DEVFREQ_MSM_CPUBW,
+	DEVFREQ_MSM_CPU_DDR_BW,
 	DEVFREQ_MAX
 };
 
@@ -16,18 +17,17 @@ enum df_device {
 void devfreq_boost_kick(enum df_device device);
 void devfreq_boost_kick_max(enum df_device device, unsigned int duration_ms);
 void devfreq_register_boost_device(enum df_device device, struct devfreq *df);
+bool df_boost_within_input(unsigned long timeout_ms);
 #else
 static inline
-void devfreq_boost_kick(enum df_device device)
-{
-}
+void devfreq_boost_kick(enum df_device device) { }
 static inline
-void devfreq_boost_kick_max(enum df_device device, unsigned int duration_ms)
-{
-}
+void devfreq_boost_kick_max(enum df_device device, unsigned int duration_ms) { }
 static inline
-void devfreq_register_boost_device(enum df_device device, struct devfreq *df)
+void devfreq_register_boost_device(enum df_device device, struct devfreq *df) { }
+static inline bool df_boost_within_input(unsigned long timeout_ms)
 {
+	return true;
 }
 #endif
 
