5c04b6e1bf7b87f4b817a4be005980f1305e872b
Qais Yousef <qais.yousef@arm.com>
2023-04-18 15:09:34 +0100

    sched/uclamp: Make task_fits_capacity() use util_fits_cpu()
    commit b48e16a69792b5dc4a09d6807369d11b2970cc36 upstream.
    
    So that the new uclamp rules in regard to migration margin and capacity
    pressure are taken into account correctly.
    
    Fixes: a7008c07a568 ("sched/fair: Make task_fits_capacity() consider uclamp restrictions")
    Co-developed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Signed-off-by: Qais Yousef <qais.yousef@arm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lore.kernel.org/r/20220804143609.515789-3-qais.yousef@arm.com
    (cherry picked from commit b48e16a69792b5dc4a09d6807369d11b2970cc36)
    Signed-off-by: Qais Yousef (Google) <qyousef@layalina.io>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4603,6 +4603,14 @@ static inline int util_fits_cpu(unsigned long util,
 	return fits;
 }
 
+static inline int task_fits_cpu(struct task_struct *p, int cpu)
+{
+	unsigned long uclamp_min = uclamp_eff_value(p, UCLAMP_MIN);
+	unsigned long uclamp_max = uclamp_eff_value(p, UCLAMP_MAX);
+	unsigned long util = task_util_est(p);
+	return util_fits_cpu(util, uclamp_min, uclamp_max, cpu);
+}
+
 static inline bool task_fits_max(struct task_struct *p, int cpu)
 {
 #ifdef CONFIG_SCHED_WALT
@@ -4624,7 +4632,7 @@ static inline bool task_fits_max(struct task_struct *p, int cpu)
 			return false;
 	}
 
-	return task_fits_capacity(p, capacity, cpu);
+        return task_fits_cpu(p, cpu);
 #else
 	return false;
 #endif
@@ -4639,7 +4647,7 @@ static inline bool task_demand_fits(struct task_struct *p, int cpu)
 	if (capacity == max_capacity)
 		return true;
 
-	return task_fits_capacity(p, capacity, cpu);
+        return task_fits_cpu(p, cpu);
 #else
 	return false;
 #endif
@@ -4714,7 +4722,7 @@ static inline void update_misfit_status(struct task_struct *p, struct rq *rq)
 #ifdef CONFIG_SCHED_WALT
 	if (task_fits_max(p, cpu_of(rq))) {
 #else
-        if (task_fits_capacity(p, cpu_of(rq))) {
+	if (task_fits_cpu(p, cpu_of(rq))) {
 #endif
 		rq->misfit_task_load = 0;
 		return;
