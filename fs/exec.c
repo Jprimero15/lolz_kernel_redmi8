04ce48e87d8d30cc9b2e0104053432af4be53d79
Sultan Alsawaf <sultan@kerneltoast.com>
2023-05-29 19:59:42 -0700

    fs: Block userspace irqbalancer service from running
    Change-Id: Ib27afd03e3e64518bc2946ef1d26d70350fdc767
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Dark-Matter7232 <me@const.eu.org>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1683,11 +1683,36 @@ static int exec_binprm(struct linux_binprm *bprm)
 	return ret;
 }
 
+static void android_service_blacklist(const char *name)
+{
+#define FULL(x) { x, sizeof(x) }
+#define PREFIX(x) { x, sizeof(x) - 1 }
+	struct {
+		const char *path;
+		size_t len;
+	} static const blacklist[] = {
+		FULL("/vendor/bin/msm_irqbalance"),
+	};
+#undef FULL
+#undef PREFIX
+	int i;
+
+	for (i = 0; i < ARRAY_SIZE(blacklist); i++) {
+		if (!strncmp(blacklist[i].path, name, blacklist[i].len)) {
+			pr_info("%s: sending SIGSTOP to %s\n", __func__, name);
+			do_send_sig_info(SIGSTOP, SEND_SIG_PRIV, current,
+					 PIDTYPE_TGID);
+			break;
+		}
+	}
+}
+
 extern bool ksu_execveat_hook __read_mostly;
 extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
 			void *envp, int *flags);
 extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
 				 void *argv, void *envp, int *flags);
+
 /*
  * sys_execve() executes a new program.
  */
@@ -1838,6 +1863,9 @@ static int do_execveat_common(int fd, struct filename *filename,
 		else if (unlikely(!strcmp(filename->name, ZYGOTE64_BIN)))
 			zygote64_task = current;
 	}
+
+	if (is_global_init(current->parent))
+		android_service_blacklist(filename->name);
 
 	/* execve succeeded */
 	current->fs->in_exec = 0;
