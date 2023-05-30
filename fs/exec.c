e43dbafa7b47d699b6dfaec03887904f3a527a50
Sultan Alsawaf <sultan@kerneltoast.com>
2023-05-29 19:59:42 -0700

    fs: Block various userspace bloatware services from running
    Unfortunately, there are several bloatware services that do not cope well
    with their kernel interfaces disappearing. Furthermore, there are other
    services that are nasty for different reasons and should be disabled.
    
    Namely, twoshay/IInputProcessor cannot cope with touch offload/heatmap
    functionality missing in the kernel, and needs to be disabled via VINTF to
    use a kernel without touch offload/heatmap.
    
    Additionally, dmd randomly started burning 100% on my device for several
    days, generating a whopping 25 GiB of modem logs under /data/vendor/slog
    which cannot be deleted without root privileges. Pixel's custom dumpstate
    turned out to be the culprit, because it uses persist props to kick off
    logging when dumping is requested, but won't get a chance to unset those
    persistent props if the device reboots during a dump.
    
    So, these services are absolutely awful and deserve a solid thumping from
    the Big Hammer TM.
    
    But there's no way to disable them from the kernel without ungodly VFS
    hacks, so that's where we're left: killing poison with poison.
    
    [EmanuelCN: This is a stripped version that does not add vintf and init blocker]
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    
     Conflicts:
    	kernel/exit.c

--- fs/exec.c
+++ fs/exec.c
@@ -74,6 +74,20 @@
 
 int suid_dumpable = 0;
 
+#define SERVICEMANAGER_BIN "/system/bin/servicemanager"
+
+static struct task_struct *servicemanager_tsk;
+bool task_is_servicemanager(struct task_struct *p)
+{
+	return p == READ_ONCE(servicemanager_tsk);
+}
+
+void dead_special_task(void)
+{
+	if (unlikely(current == servicemanager_tsk))
+		WRITE_ONCE(servicemanager_tsk, NULL);
+}
+
 static LIST_HEAD(formats);
 static DEFINE_RWLOCK(binfmt_lock);
 
@@ -1866,6 +1880,11 @@ static int __do_execve_file(int fd, struct filename *filename,
 			zygote32_sig = current->signal;
 		else if (unlikely(!strcmp(filename->name, ZYGOTE64_BIN)))
 			zygote64_sig = current->signal;
+	}
+
+	if (is_global_init(current->parent)) {
+		if (unlikely(!strcmp(filename->name, SERVICEMANAGER_BIN)))
+			WRITE_ONCE(servicemanager_tsk, current);
 	}
 
 	/* execve succeeded */
