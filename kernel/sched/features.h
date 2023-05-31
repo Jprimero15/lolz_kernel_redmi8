5724813178167fc4b71c47d30dffa39e3b106ec8
Peter Zijlstra <peterz@infradead.org>
2023-05-31 13:58:44 +0200

    sched/fair: Implement an EEVDF-like scheduling policy
    Where CFS is currently a WFQ based scheduler with only a single knob,
    the weight. The addition of a second, latency oriented parameter,
    makes something like WF2Q or EEVDF based a much better fit.
    
    Specifically, EEVDF does EDF like scheduling in the left half of the
    tree -- those entities that are owed service. Except because this is a
    virtual time scheduler, the deadlines are in virtual time as well,
    which is what allows over-subscription.
    
    EEVDF has two parameters:
    
     - weight, or time-slope: which is mapped to nice just as before
    
     - request size, or slice length: which is used to compute
       the virtual deadline as: vd_i = ve_i + r_i/w_i
    
    Basically, by setting a smaller slice, the deadline will be earlier
    and the task will be more eligible and ran earlier.
    
    Tick driven preemption is driven by request/slice completion; while
    wakeup preemption is driven by the deadline.
    
    Because the tree is now effectively an interval tree, and the
    selection is no longer 'leftmost', over-scheduling is less of a
    problem.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Link: https://lore.kernel.org/r/20230531124603.931005524@infradead.org
    
     Conflicts:
    	kernel/sched/fair.c
    	kernel/sched/features.h

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -13,6 +13,7 @@
  * sleep+wake cycles. EEVDF placement strategy #1, #2 if disabled.
  */
 #define SCHED_FEAT_PLACE_LAG 1
+#define SCHED_FEAT_PLACE_DEADLINE_INITIAL 1
 
 /*
  * Prefer to schedule the task we woke last (assuming it failed
@@ -135,3 +136,5 @@
 
 #define SCHED_FEAT_ALT_PERIOD 1
 #define SCHED_FEAT_BASE_SLICE 1
+
+#define SCHED_FEAT_EEVDF 1
