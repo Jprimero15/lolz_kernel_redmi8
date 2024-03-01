ee369d24ee969df5b3dc6e3df73a6c15abb603b1
Holger Hoffstätte <holger.hoffstaette@googlemail.com>
2024-03-01 09:30:25 +0800

    loop: properly observe rotational flag of underlying device
    [ Upstream commit 56a85fd8376ef32458efb6ea97a820754e12f6bb ]
    
    The loop driver always declares the rotational flag of its device as
    rotational, even when the device of the mapped file is nonrotational,
    as is the case with SSDs or on tmpfs. This can confuse filesystem tools
    which are SSD-aware; in my case I frequently forget to tell mkfs.btrfs
    that my loop device on tmpfs is nonrotational, and that I really don't
    need any automatic metadata redundancy.
    
    The attached patch fixes this by introspecting the rotational flag of the
    mapped file's underlying block device, if it exists. If the mapped file's
    filesystem has no associated block device - as is the case on e.g. tmpfs -
    we assume nonrotational storage. If there is a better way to identify such
    non-devices I'd love to hear them.
    
    Cc: Jens Axboe <axboe@kernel.dk>
    Cc: linux-block@vger.kernel.org
    Cc: holger@applied-asynchrony.com
    Signed-off-by: Holger Hoffstätte <holger.hoffstaette@googlemail.com>
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>
    Signed-off-by: Benjamin Gordon <bmgordon@chromium.org>
    Reviewed-by: Guenter Roeck <groeck@chromium.org>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Genjian Zhang <zhanggenjian@kylinos.cn>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    [Adapt to our kernel and remove additional loop_config_discard(lo) to align with upstream]
    Signed-off-by: Joshua Primero <jprimero15@aospa.co>

--- drivers/block/loop.c
+++ drivers/block/loop.c
@@ -947,6 +947,24 @@ static int loop_prepare_queue(struct loop_device *lo)
 	return 0;
 }
 
+static void loop_update_rotational(struct loop_device *lo)
+{
+	struct file *file = lo->lo_backing_file;
+	struct inode *file_inode = file->f_mapping->host;
+	struct block_device *file_bdev = file_inode->i_sb->s_bdev;
+	struct request_queue *q = lo->lo_queue;
+	bool nonrot = true;
+
+	/* not all filesystems (e.g. tmpfs) have a sb->s_bdev */
+	if (file_bdev)
+		nonrot = blk_queue_nonrot(bdev_get_queue(file_bdev));
+
+	if (nonrot)
+		blk_queue_flag_set(QUEUE_FLAG_NONROT, q);
+	else
+		blk_queue_flag_clear(QUEUE_FLAG_NONROT, q);
+}
+
 static int
 loop_release_xfer(struct loop_device *lo)
 {
@@ -1131,7 +1149,7 @@ static int loop_configure(struct loop_device *lo, fmode_t mode,
 	blk_queue_physical_block_size(lo->lo_queue, bsize);
 	blk_queue_io_min(lo->lo_queue, bsize);
 
-	loop_config_discard(lo);
+	loop_update_rotational(lo);
 	loop_update_dio(lo);
 	loop_sysfs_init(lo);
 
