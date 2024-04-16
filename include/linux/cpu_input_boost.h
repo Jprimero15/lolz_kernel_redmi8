ecef9246af4e152817560efd2923714ff0c292a4
Joshua Primero <jprimero15@aospa.co>
2024-04-16 14:15:26 +0000

    drivers: cpu_input_boost: Sync with LOLZ v24

--- include/linux/cpu_input_boost.h
+++ include/linux/cpu_input_boost.h
@@ -1,9 +1,12 @@
-/* SPDX-License-Identifier: GPL-2.0
- *
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
  * Copyright (C) 2018-2019 Sultan Alsawaf <sultan@kerneltoast.com>.
+ * Copyright (C) 2018-2021 LOLZ Kernel
  */
 #ifndef _CPU_INPUT_BOOST_H_
 #define _CPU_INPUT_BOOST_H_
+
+#include <linux/types.h>
 
 #ifdef CONFIG_CPU_INPUT_BOOST
 void cpu_input_boost_kick(void);
