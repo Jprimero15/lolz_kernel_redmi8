0d3fe5d75548d7fefe53bfbc88717904180a8bce
EmanuelCN <emanuelghub@gmail.com>
2024-10-21 22:27:59 +0300

    Revert "timer: Add a global deferrable timer"
    This reverts commit a45c402c89f23423279b255e697853c3c93c8ff2.
    
     Conflicts:
    	include/linux/timer.h

--- include/linux/timer.h
+++ include/linux/timer.h
@@ -177,8 +177,6 @@ extern void timer_quiesce_cpu(void *cpup);
 
 extern void add_timer(struct timer_list *timer);
 
-extern struct timer_base timer_base_deferrable;
-
 extern int try_to_del_timer_sync(struct timer_list *timer);
 
 extern int del_timer_sync(struct timer_list *timer);
