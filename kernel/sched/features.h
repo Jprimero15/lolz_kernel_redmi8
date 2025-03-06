8c414313d0571fbb5ad2ce8bdb7866fbcca4ab19
EmanuelCN <emanuelghub@gmail.com>
2025-03-06 21:56:12 +0200

    kernel/sched: Disable SCHED_FEAT_PLACE_REL_DEADLINE
    This is not needed for android nor we backported virtual deadline changes

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -12,7 +12,7 @@
 /*
  * Preserve relative virtual deadline on 'migration'.
  */
-#define SCHED_FEAT_PLACE_REL_DEADLINE 1
+#define SCHED_FEAT_PLACE_REL_DEADLINE 0
 /*
  * Inhibit (wakeup) preemption until the current task has either matched the
  * 0-lag point or until is has exhausted it's slice.
