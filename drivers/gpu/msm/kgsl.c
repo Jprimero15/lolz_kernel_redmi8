96914da5a1ed48f91ac43c85cf666eae7bcfb54e
Puranam V G Tejaswi <quic_pvgtejas@quicinc.com>
2025-06-24 12:46:00 +0300

    msm: kgsl: Use kthread instead of workqueue for event work
    Currently a workqueue is being used to process the event work. In certain
    scenarios like when most of CPU cores are busy, there can be a significant
    delay between the actual timestamp retire event and when the work is
    processed by the events workqueue as workqueues cannot have RT priority.
    Hence use kthread instead of workqueue for event work.
    
    [EmanuelCN: Backport to msm-4.19]
    Change-Id: Ib1ec7fa1ec3a133d03104c9a029dcc4c06180609
    Signed-off-by: Puranam V G Tejaswi <quic_pvgtejas@quicinc.com>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -608,11 +608,11 @@ int kgsl_context_init(struct kgsl_device_private *dev_priv,
 	if (id == -ENOSPC) {
 		/*
 		 * Before declaring that there are no contexts left try
-		 * flushing the event workqueue just in case there are
+		 * flushing the event worker just in case there are
 		 * detached contexts waiting to finish
 		 */
 
-		flush_workqueue(device->events_wq);
+		kthread_flush_worker(device->events_worker);
 		id = _kgsl_get_context_id(device);
 	}
 
@@ -5327,6 +5327,7 @@ int kgsl_of_property_read_ddrtype(struct device_node *node, const char *base,
 int kgsl_device_platform_probe(struct kgsl_device *device)
 {
 	int status = -EINVAL;
+	struct sched_param param = { .sched_priority = 16 };
 
 	status = _register_device(device);
 	if (status)
@@ -5413,8 +5414,8 @@ int kgsl_device_platform_probe(struct kgsl_device *device)
 				PM_QOS_CPU_DMA_LATENCY,
 				PM_QOS_DEFAULT_VALUE);
 
-	device->events_wq = alloc_workqueue("kgsl-events",
-		WQ_UNBOUND | WQ_MEM_RECLAIM | WQ_SYSFS | WQ_HIGHPRI, 0);
+	device->events_worker = kthread_create_worker(0, "kgsl-events");
+	sched_setscheduler(device->events_worker->task, SCHED_FIFO, &param);
 
 	/* Initialize the snapshot engine */
 	kgsl_device_snapshot_init(device);
@@ -5437,7 +5438,7 @@ EXPORT_SYMBOL(kgsl_device_platform_probe);
 
 void kgsl_device_platform_remove(struct kgsl_device *device)
 {
-	destroy_workqueue(device->events_wq);
+	kthread_destroy_worker(device->events_worker);
 
 	kfree(device->dev->dma_parms);
 	device->dev->dma_parms = NULL;
