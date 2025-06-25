d8a2ae18969ecde4251fa60cfa2464c5af37cd18
EmanuelCN <emanuelghub@gmail.com>
2025-06-25 12:43:24 +0300

    cpufreq: schedutil: Reduce DVFS headroom boost
    The tapering is now capacity-aware, giving little cores a larger boost.
    This requires taming the overall util boost to improve efficiency.

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -362,7 +362,7 @@ static inline unsigned long apply_dvfs_headroom(unsigned long util, int cpu)
          * capacity
          */
 	delta = capacity - util;
-	headroom = (delta * delta) / (4 * capacity);
+	headroom = (delta * delta) / (5 * capacity);
 
 	/* 10% of capacity threshold */
     	min_util = capacity / 10;
